9d6ce425cb19e46b55872e80b8d99272f896c853
sanikhan <sanikhan.cse@gmail.com>
2021-07-06 01:53:43 +0600

    bootstrap from node

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './components/components'
+import 'bootstrap'
+import jQuery from 'jquery'
+window.$ = jQuery
 
 Vue.prototype.$hub = new Vue();
 

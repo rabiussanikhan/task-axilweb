ec24b991c6b4fa73dd5bca903edb4b45879455bf
sanikhan <sanikhan.cse@gmail.com>
2021-07-06 17:38:15 +0600

    updated

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -95,7 +95,58 @@
                                         />
                                         <div v-if="item.product" class="burger-menu-wrapper">
                                             <div class="burger-menu">
-
+                                                <div class="row p-5">
+                                                    <div class="col-4">
+                                                        <h3 class="menu-heading mb-0">Product</h3>
+                                                        <ul class="list-unstyled">
+                                                            <li class="single-item mt-4">Product 001</li>
+                                                            <li class="single-item mt-4">Item 002</li>
+                                                            <li class="single-item mt-4">Product item 003</li>
+                                                        </ul>
+                                                        <h3 class="menu-heading mt-4">Product</h3>
+                                                        <ul class="list-unstyled">
+                                                            <li class="single-item mt-4">Product 001</li>
+                                                            <li class="single-item mt-4">Item 002</li>
+                                                            <li class="single-item mt-4">Product item 003</li>
+                                                            <li class="single-item mt-4">Product 004</li>
+                                                        </ul>
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <h3 class="menu-heading mb-0">Product</h3>
+                                                        <ul class="list-unstyled">
+                                                            <li class="single-item mt-4">Product 001</li>
+                                                            <li class="single-item mt-4">Item 002</li>
+                                                            <li class="single-item mt-4">Product item 003</li>
+                                                            <li class="single-item mt-4">Product 004</li>
+                                                            <li class="single-item mt-4">Item 005</li>
+                                                            <li class="single-item mt-4">Product item 006</li>
+                                                        </ul>
+                                                    </div>
+                                                    <div class="col-4 feature-wrapper">
+                                                        <div class="divider"></div>
+                                                        <h3 class="menu-heading mb-0">Featured</h3>
+                                                        <div class="d-flex flex-column">
+                                                            <img class="feature-img img-fluid"
+                                                                 :src="urlGenerator(`images/burger-menu/item-1.png`)"
+                                                                 alt="Not Found"/>
+                                                            <img class="feature-img img-fluid"
+                                                                 :src="urlGenerator(`images/burger-menu/item-2.png`)"
+                                                                 alt="Not Found"/>
+                                                        </div>
+                                                        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-xxl-between">
+                                                            <img class="feature-img img-fluid"
+                                                                 :src="urlGenerator(`images/burger-menu/item-3.png`)"
+                                                                 alt="Not Found"/>
+                                                            <img class="feature-img img-fluid"
+                                                                 :src="urlGenerator(`images/burger-menu/item-4.png`)"
+                                                                 alt="Not Found"/>
+                                                        </div>
+                                                        <button type="button"
+                                                                class="see-all-btn btn btn-primary shadow-none">
+                                                            See All Offers
+                                                        </button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </li>
